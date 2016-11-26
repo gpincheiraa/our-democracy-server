b3f2a426b94d756ff0f3db961943f7b07b86c51d
benjablanko <benjaminblancoseibert@gmail.com>
2016-11-26 16:10:47 -0300

    refactor arrow function

--- src/server/controllers/monkey.controller.js
+++ src/server/controllers/monkey.controller.js
@@ -6,22 +6,23 @@ const config = require('../../config/env');
 
 function get(req, res, next) {
   if (req.body.search) {
-    var ml = new MonkeyLearn('dd137db1d75032bb63044096b8f9cb6e81296030');
-    var module_id = 'cl_u9PRHNzf';
-    var text_list = ["hola como estas", "maldito hijo de perra", "que mujer mas guapa"];
-    var p = ml.classifiers.classify(module_id, text_list, true);
+    let ml = new MonkeyLearn('dd137db1d75032bb63044096b8f9cb6e81296030');
+    let module_id = 'cl_u9PRHNzf';
+    let text_list = ["hola como estas", "maldito hijo de perra", "que mujer mas guapa"];
+    let p = ml.classifiers.classify(module_id, text_list, true);
 
-    p.then(function (_res) {
-      console.log(_res.result);
-      return res.json({
-        list: _res.result,
-        status: {
-          'message': 'OK',
-          'code' : httpStatus.OK
-        }
+    p
+      .then( (_res) => {
+        console.log(_res.result);
+        return res.json({
+          list: _res.result,
+          status: {
+            'message': 'OK',
+            'code' : httpStatus.OK
+          }
+        });
+
       });
-
-    });
 
 
   }
