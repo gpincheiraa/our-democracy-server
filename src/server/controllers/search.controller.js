2374cd4d3052eb1e0f15def192f8115351e5fe24
Ivan Olivares R <hola@ivolivares.cl>
2016-11-26 17:17:00 -0300

    search on twitter :smile:

--- src/server/controllers/search.controller.js
+++ src/server/controllers/search.controller.js
@@ -1,30 +1,41 @@
 import httpStatus from 'http-status';
 import APIError from '../helpers/APIError';
 import Twitter from 'twitter'
+
 const config = require('../../config/env');
-/*const client = new Twitter({
-  consumer_key: config.auth.twitter.CONSUMER_KEY,
-  consumer_secret: config.auth.twitter.CONSUMER_SECRET,
-  access_token_key: req.session.oauthRequestToken || '',
-  access_token_secret: req.session.oauthRequestTokenSecret || ''
-});*/
 
 let search = (req, res, next) => {
   if (req.params.q) {
-   /* client.get('search/tweets', {q: req.params.q}, (error, tweets, response) => {
-      console.log(tweets);
-    });*/
-    return res.json({
-      searched: req.params.q,
+
+    const client = new Twitter({
+      consumer_key: config.auth.twitter.CONSUMER_KEY,
+      consumer_secret: config.auth.twitter.CONSUMER_SECRET,
+      access_token_key: req.session.oauthRequestToken || '',
+      access_token_secret: req.session.oauthRequestTokenSecret || ''
+    });
+
+    client.get('search/tweets', { q: req.params.q }, (error, tweets, response) => {
+      processTweets(tweets, res);
+    });
+  }
+};
+
+let processTweets = (tweets, response) => {
+  if(tweets) {
+    let tweetTexts = [];
+    tweets.statuses.forEach( (tweet) => {
+      tweetTexts.push(tweet.text);
+    });
+
+    return response.json({
+      search_string: '',
+      data: tweetTexts,
       status: {
         'message': 'OK',
         'code' : httpStatus.OK
       }
     });
   }
-
-  const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED);
-  return next(err);
 };
 
 export default { search };
