f511fa5f226f4bc97040cfc9396747702d07ee03
benjablanko <benjaminblancoseibert@gmail.com>
2016-11-26 16:46:15 -0300

    add twiter and fix search

--- src/server/controllers/search.controller.js
+++ src/server/controllers/search.controller.js
@@ -1,19 +1,19 @@
 import httpStatus from 'http-status';
 import APIError from '../helpers/APIError';
-
+import Twitter from 'twitter'
 const config = require('../../config/env');
-const client = new Twitter({
+/*const client = new Twitter({
   consumer_key: config.auth.twitter.CONSUMER_KEY,
   consumer_secret: config.auth.twitter.CONSUMER_SECRET,
   access_token_key: req.session.oauthRequestToken || '',
   access_token_secret: req.session.oauthRequestTokenSecret || ''
-});
+});*/
 
 let search = (req, res, next) => {
   if (req.params.q) {
-    client.get('search/tweets', {q: req.params.q}, (error, tweets, response) => {
+   /* client.get('search/tweets', {q: req.params.q}, (error, tweets, response) => {
       console.log(tweets);
-    });
+    });*/
     return res.json({
       searched: req.params.q,
       status: {
